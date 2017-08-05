3747b75589a818433cb22166eb092631a2f2840c
Guillaume Lessard <dhtnstff@gmail.com>
2017-08-04 23:51:09 -0600

    package description overhaul

--- Package.swift
+++ Package.swift
@@ -1,5 +1,31 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
-let package = Package(
-    name: "composition"
-)
+let name = "composition"
+
+#if swift(>=4.0)
+
+  let package = Package(
+    name: name,
+    products: [
+      .library(name: name, targets: [name]),
+    ],
+    targets: [
+      .target(
+        name: name,
+        path: "Sources"),
+      .testTarget(
+        name: name+"Tests",
+        dependencies: [Target.Dependency(stringLiteral: name)]),
+    ],
+    swiftLanguageVersions: [3,4]
+  )
+
+#else
+
+  let package = Package(
+    name: name
+  )
+
+#endif
